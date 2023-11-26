28e6fc5653f2a39ccbce15094a3ae92a55e4040e
HAMZA-NASIR-PU <60808055+HAMZA-NASIR-PU@users.noreply.github.com>
2023-11-27 02:45:31 +0500

    sayHello function

--- Main.java
+++ Main.java
@@ -2,5 +2,10 @@ public class Main{
     public static void main(String[] args) {
         System.out.println("HELLO WORLD from Java Program");
         System.out.println("Added one more line...");
+        sayHello();
+    }
+
+    public static void sayHello(){
+        System.out.println("Hi, I am student...");
     }
 }
